872805db4488b7a839342deb3b48149c10ad6a8a
Alexey Pozhidaev <worka@bk.ru>
2020-11-02 10:57:47 +0300

    Templates users, clear comments

--- common/fixtures/templates/users.php
+++ common/fixtures/templates/users.php
@@ -8,16 +8,8 @@
 return [
     'city_id' => $faker->numberBetween(1, 1108),
     'profile_id' => $index + 1,
-    // 'about' => $faker->text(60),
-    // 'first_name' => $faker->firstName($gender),
-    // 'last_name' => $faker->lastName($gender),
     'email' => $faker->unique()->email,
-    // 'gender' => $gender,
     'created_at' => $faker->dateTimeBetween('-20 days', 'now')->format('Y-m-d H:i:s'),
-    // 'birth_date' => $faker->dateTimeInInterval('-30 years', '+5 years')->format('Y-m-d H:i:s'),
     'last_logined_at' => $faker->dateTimeInInterval('-3 days')->format('Y-m-d H:i:s'),
-    // 'role' => $faker->numberBetween(1, 2),
-    'password_hash' => \Yii::$app->security->generatePasswordHash("password_{$index}", 4),
-    // 'phone' => $faker->phoneNumber,
-    // 'skype' => $faker->text(5)
+    'password_hash' => \Yii::$app->security->generatePasswordHash("password_{$index}", 4)
 ];
