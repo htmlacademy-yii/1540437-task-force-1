d2f89a2f4b78c9e1f09511200be53259581a1df4
Alexet Pozhidaev <worka@bk.ru>
2021-05-29 17:53:26 +0300

    Signin form

---
+++ frontend/models/forms/SigninForm.php
@@ -0,0 +1,48 @@
+<?php
+namespace frontend\models\forms;
+
+use common\models\UserIdentity as User;
+
+class SigninForm extends \yii\base\Model
+{
+    public $email;
+    public $password;
+
+    /** @var User $_user */
+    private $_user;
+
+    /** @inheritDoc */
+    public function rules()
+    {
+        return [
+            [['email', 'password'], 'required'],
+            ['email', 'email'],
+            ['password', 'validatePassword'],
+        ];
+    }
+
+    /**
+     * 
+     * @param string $attribute Attribte name
+     * @param mixed $params 
+     * @return void 
+     */
+    public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Неправильный email или пароль');
+            }
+        }
+    }
+
+    private function getUser()
+    {
+        if ($this->_user === null) {
+            $this->_user = User::findOne(['email' => $this->email]);
+        }
+
+        return $this->_user;
+    }
+}
